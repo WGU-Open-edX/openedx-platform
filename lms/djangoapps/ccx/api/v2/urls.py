92e0ac5c8a6af3957ff5917fdfbfe3ff413da38d
Brian Buck <brian.buck@wgu.edu>
2026-08-26 15:34:52 -0600

    feat: Create the CCX v2 get_tabs() endpoint
    Also adds basic CCX creation scaffolding

---
+++ lms/djangoapps/ccx/api/v2/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.urls import re_path
+
+from lms.djangoapps.ccx.api.v2 import views
+
+app_name = 'ccx_coach_v2'
+urlpatterns = [
+    re_path(
+        fr'^courses/{settings.COURSE_ID_PATTERN}/metadata$',
+        views.CCXCoachMetadataView.as_view(),
+        name='metadata',
+    ),
+    re_path(
+        fr'^courses/{settings.COURSE_ID_PATTERN}/create_ccx$',
+        views.CreateCCXView.as_view(),
+        name='create_ccx',
+    ),
+]
